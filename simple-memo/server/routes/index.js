cfc6efd8a6c9f7f092d102ce4ae7208642bdfec7
HwangEyiKwon <hwang21035@naver.com>
2018-11-04 17:47:51 +0900

    20181104 HEK login to index router

---
+++ simple-memo/server/routes/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+var passport = require('passport');
+const passportConfig = require('../../config/passport');
+
+router.post('/login', function(req,res,next){
+  passport.authenticate('login', function (err, user, info) {
+//    console.log(user+"asdfasdf");
+    if(err) console.log(err);
+    if(user) res.send({success: true});
+    else res.send({success: false});
+  })(req,res,next);
+});
+
+router.post('/signup', function (req, res, next) {
+
+  passport.authenticate('signup', function (err, user, info) {
+    // console.log(user+"s");
+    console.log("signUPPPPPPPP");
+    if(err) console.log(err);
+    if(user) res.send({success: true});
+    else res.send({success: false});
+  })(req,res,next);
+});
+
+
+router.post('/getUserInfo', function (req,res) {
+
+  console.log("get User Info: "+JSON.stringify(req.body));
+
+  var email = req.body.email ;
+
+  user.fineOne({email: email}, function(err, info){
+    if(err) console.log(err);
+    if(info == null) {
+      console.log("사용자 아님");
+    }
+    else {
+      console.log("사용자 찾음");
+      res.send(info);
+    }
+  })
+})
+
+module.exports = router ;
