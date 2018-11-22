32dbf4a934d9f03832e1662f88f4e34b7f79d042
HwangEyiKwon <hwang21035@naver.com>
2018-11-22 18:47:59 +0900

    2018-11-22 HEK intermediate presentation_server

--- NodeServerProjects/server/models/content.js
+++ NodeServerProjects/server/models/content.js
@@ -9,7 +9,7 @@ const contentSchema = new Schema({
   endDate: Date,
   achievementRate: Number,
   totalUser: Number,
-  userList: [{name: String, email: String, newVideo: {path: String, authen: Number, authorizePeople: [{name: String, authenInfo: Number}] } }], //authenInfo: 0: 실패 1: 성공 / authon: 0: 인증 실패 1: 인증 성공 2: 인증 Not yet
+  userList: [{name: String, email: String, newVideo: {path: String, authen: Number, authorizePeople: [{email: String, authenInfo: Number}] } }], //authenInfo: 0: 실패 1: 성공 / authon: 0: 인증 실패 1: 인증 성공 2: 인증 Not yet
   isDone: Number, // 0: 진행중 1: 종료 2: 시작 전
   description: String,
 });
