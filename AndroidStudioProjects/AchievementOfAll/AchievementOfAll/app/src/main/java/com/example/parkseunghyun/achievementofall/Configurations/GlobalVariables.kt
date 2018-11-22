ef45e5ad1a54a882e3d8002042e6616c6852cce9
park-seung-hyun <shp9408@gmail.com>
2018-11-22 15:24:10 +0900

    2018/11/21 PSH Edit

--- AndroidStudioProjects/AchievementOfAll/AchievementOfAll/app/src/main/java/com/example/parkseunghyun/achievementofall/Configurations/GlobalVariables.kt
+++ AndroidStudioProjects/AchievementOfAll/AchievementOfAll/app/src/main/java/com/example/parkseunghyun/achievementofall/Configurations/GlobalVariables.kt
@@ -5,5 +5,5 @@ import android.app.Application
 class GlobalVariables : Application() {
 
     // 서버 ip 주소 전역변수 설정
-    var ipAddress: String = "http://192.168.8.97:3000"
+    var ipAddress: String = "http://192.168.3.211:3000"
 }
