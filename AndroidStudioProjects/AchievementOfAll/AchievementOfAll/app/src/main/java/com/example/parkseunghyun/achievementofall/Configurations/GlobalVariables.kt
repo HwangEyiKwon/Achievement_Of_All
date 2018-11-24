8bc6b8ca48a76e0e490b63182cdf9e3c35346356
“geonheec” <geonheec0124@gmail.com>
2018-11-24 16:52:55 +0900

    2018/11/24 CGH Updating Remaining Time

--- AndroidStudioProjects/AchievementOfAll/AchievementOfAll/app/src/main/java/com/example/parkseunghyun/achievementofall/Configurations/GlobalVariables.kt
+++ AndroidStudioProjects/AchievementOfAll/AchievementOfAll/app/src/main/java/com/example/parkseunghyun/achievementofall/Configurations/GlobalVariables.kt
@@ -5,6 +5,6 @@ import android.app.Application
 class GlobalVariables : Application() {
 
     // 서버 ip 주소 전역변수 설정
-    var ipAddress: String = "http://192.168.0.27:3000"
+    var ipAddress: String = "http://192.168.0.8:3000"
 
 }
