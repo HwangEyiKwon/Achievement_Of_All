84edc7da3790adca396156e33bd14f3a289a9166
park-seung-hyun <shp9408@gmail.com>
2018-11-30 22:54:16 +0900

    2018/11/30 PSH penalty UI

---
+++ AndroidStudioProjects/AchievementOfAll/AchievementOfAll/app/src/main/java/com/example/parkseunghyun/achievementofall/Activities/PenaltyActivity.kt
@@ -0,0 +1,42 @@
+package com.example.parkseunghyun.achievementofall.Activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.example.parkseunghyun.achievementofall.R
+
+class PenaltyActivity : AppCompatActivity() {
+
+    var jwtToken: String ?= null
+    var contentName: String ?= null
+
+    var currentMoney: TextView ?= null
+    var rewardMoney: TextView ?= null
+    var finalMoney: TextView ?= null
+
+    var cm: Int ?= null
+    var rm: Int ?= null
+    var fm: Int ?= null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reward)
+
+
+        currentMoney = findViewById(R.id.current_money)
+        rewardMoney = findViewById(R.id.final_reward)
+        finalMoney = findViewById(R.id.final_money)
+
+        cm = intent.getIntExtra("currentMoney",0)
+        rm = intent.getIntExtra("rewardMoney",0)
+        fm = cm!!+rm!!
+
+        currentMoney!!.text = cm.toString()
+        rewardMoney!!.text = rm.toString()
+        finalMoney!!.text = fm.toString()
+
+
+    }
+
+}
